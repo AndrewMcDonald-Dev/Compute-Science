33040b56fea22bbef6e9c30855b372c135195016
Andrew McDonald <andrewmcd21301@gmail.com>
2022-11-20 15:25:48 -0500

    Did all of the sorts but unsure if they are all correct

--- AdvData/sorting/src/main.rs
+++ AdvData/sorting/src/main.rs
@@ -7,14 +7,15 @@ fn main() {
         for _ in 0..n {
             test_arr.push(rand::thread_rng().gen_range(0..100000));
         }
-        let mut insertion_test = test_arr.clone();
-        let mut quick_test = test_arr.clone();
-        let mut merge_test = test_arr.clone();
-        let mut heap_test = test_arr.clone();
-        let comp_insert = insertion_sort(&mut insertion_test);
-        let comp_quick = quick_sort(&mut quick_test);
-        let (comp_merge, _) = mergesort(&mut merge_test);
-        let comp_heap = heapsort(&mut heap_test);
+        let comp_insert = insertion_sort(&mut test_arr.clone());
+        let comp_quick = quick_sort(&mut test_arr.clone(), None, None);
+        let (comp_merge, _) = mergesort(&mut test_arr.clone());
+        let comp_heap = heapsort(&mut test_arr.clone());
+        let comp_shell = shell_sort(&mut test_arr.clone());
+        let comp_hibbard = hibbard_sort(&mut test_arr.clone());
+        let comp_knuth = knuth_sort(&mut test_arr.clone());
+        let comp_gonnet = gonnet_sort(&mut test_arr.clone());
+        let comp_sedgewick = sedgewick_sort(&mut test_arr.clone());
         let is_end = n == 10000;
         output_formatter(
             [
@@ -23,47 +24,62 @@ fn main() {
                 comp_quick,
                 comp_merge,
                 comp_heap,
+                comp_shell,
+                comp_hibbard,
+                comp_knuth,
+                comp_gonnet,
+                comp_sedgewick,
                 (n as f64 * (n as f64).log2()) as usize,
             ],
             is_end,
         )
     }
 
-    // let mut test_arr = [0, 3, 2, 5, 6, 9, 1, 8, 7, 4];
-    // let comp = quick_sort(&mut test_arr);
+    // let mut test_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // let comp = hibbard_sort(&mut test_arr);
     // println!("{}, {:?}", comp, test_arr);
 }
 
-fn output_formatter(results: [usize; 6], is_end: bool) {
+fn output_formatter(results: [usize; 11], is_end: bool) {
     println!(
-        "│ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │",
-        results[0], results[1], results[2], results[3], results[4], results[5]
+        "│ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │",
+        results[0],
+        results[1],
+        results[2],
+        results[3],
+        results[4],
+        results[5],
+        results[6],
+        results[7],
+        results[8],
+		results[9],
+		results[10]
     );
     if !is_end {
         println!(
-            "├{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┤",
-            "", "", "", "", "", ""
+            "├{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┤",
+            "", "", "", "", "", "", "", "", "", "", ""
         );
     } else {
         println!(
-            "└{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┘",
-            "", "", "", "", "", ""
+            "└{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┴{:─<11}┘",
+            "", "", "", "", "", "", "", "", "", "", ""
         )
     }
 }
 
 fn initial_formatting() {
     println!(
-        "┌{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┐",
-        "", "", "", "", "", ""
+        "┌{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┬{:─<11}┐",
+        "", "", "", "", "", "", "", "", "", "", ""
     );
     println!(
-        "│ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │",
-        "N", "Insertion", "Quick", "Merge", "Heap", "NlgN"
+        "│ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │ {: ^9} │",
+        "N", "Insertion", "Quick", "Merge", "Heap", "Shell", "Hibbard", "Knuth", "Gonnet", "Sedgewick", "NlgN"
     );
     println!(
-        "├{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┤",
-        "", "", "", "", "", ""
+        "├{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┼{:─<11}┤",
+        "", "", "", "", "", "", "", "", "", "", ""
     );
 }
 
@@ -71,6 +87,7 @@ fn insertion_sort(arr: &mut Vec<i32>) -> usize {
     let mut count = 0;
     for i in 1..arr.len() {
         let mut j = i;
+        count += 1;
         while j > 0 && arr[j - 1] > arr[j] {
             arr.swap(j - 1, j);
             count += 1;
@@ -80,85 +97,53 @@ fn insertion_sort(arr: &mut Vec<i32>) -> usize {
     count
 }
 
-// fn quick_sort(values: &mut [i32], start: Option<isize>, end: Option<isize>) -> usize {
-//     fn partition(arr: &mut [i32], start: isize, end: isize) -> (usize, usize) {
-//         let mut count = 0;
-//         let pivot = end as usize;
-//         let mut store_index = start - 1;
-//         let mut last_index = end;
-//         loop {
-//             store_index += 1;
-//             while arr[store_index as usize] < arr[pivot] {
-//                 store_index += 1;
-//                 count += 1;
-//             }
-
-//             last_index -= 1;
-//             while last_index >= 0 && arr[last_index as usize] > arr[pivot] {
-//                 count += 1;
-//                 last_index -= 1;
-//             }
-//             if store_index >= last_index {
-//                 break;
-//             } else {
-//                 arr.swap(store_index as usize, last_index as usize);
-//             }
-//         }
-//         arr.swap(store_index as usize, pivot as usize);
-//         println!("test: {}", count);
-//         (store_index as usize, count)
-//     }
-//     let mut count = 0;
-
-//     let start_val = start.unwrap_or(0);
-//     let end_val = end.unwrap_or((values.len() - 1) as isize);
-//     let mut test = false;
-
-//     // println!("test: {}", count);
-//     if start_val < end_val {
-//         let (pivot, new_count) = partition(values, start_val, end_val);
-//         count += new_count;
-//         count += quick_sort(values, Some(start_val), Some(pivot as isize - 1));
-//         count += quick_sort(values, Some(pivot as isize + 1), Some(end_val));
-//         test = true;
-//     }
-//     println!("{} {}", count, test);
-//     count
-// }
-fn quick_sort(slice: &mut [i32]) -> usize {
-    fn partition(slice: &mut [i32]) -> (usize, usize) {
-        let len = slice.len();
-        let pivot = slice[len - 1];
-        let mut i = 0;
-        let mut j = 0;
+fn quick_sort(values: &mut [i32], start: Option<isize>, end: Option<isize>) -> usize {
+    fn partition(arr: &mut [i32], start: isize, end: isize) -> (usize, usize) {
         let mut count = 0;
-
-        while j < len - 1 {
-            if slice[j] <= pivot {
-                slice.swap(i, j);
-                i += 1;
-            }
+        let pivot = end as usize;
+        let mut store_index = start - 1;
+        let mut last_index = end;
+        loop {
+            store_index += 1;
             count += 1;
-            j += 1;
+            while arr[store_index as usize] < arr[pivot] {
+                store_index += 1;
+                count += 1;
+            }
+
+            last_index -= 1;
+            count += 1;
+            while last_index >= 0 && arr[last_index as usize] > arr[pivot] {
+                count += 1;
+                last_index -= 1;
+            }
+
+            if store_index >= last_index {
+                break;
+            } else {
+                arr.swap(store_index as usize, last_index as usize);
+            }
         }
-
-        slice.swap(i, len - 1);
-
-        (i, count)
+        arr.swap(store_index as usize, pivot as usize);
+        (store_index as usize, count)
     }
-    let mut count = 1;
-    if !slice.is_empty() {
-        let (partition_index, new_count) = partition(slice);
-        let len = slice.len();
+    let mut count = 0;
+
+    let start_val = start.unwrap_or(0);
+    let end_val = end.unwrap_or((values.len() - 1) as isize);
+
+    // println!("test: {}", count);
+    if start_val < end_val {
+        let (pivot, new_count) = partition(values, start_val, end_val);
         count += new_count;
-
-        count += quick_sort(&mut slice[0..partition_index]);
-        count += quick_sort(&mut slice[partition_index + 1..len]);
+        count += quick_sort(values, Some(start_val), Some(pivot as isize - 1));
+        count += quick_sort(values, Some(pivot as isize + 1), Some(end_val));
     }
-    count + 1
+    count
 }
 
-fn shell_sort(arr: &mut [i32]) {
+fn shell_sort(arr: &mut [i32]) -> usize {
+    let mut count = 0;
     let len = arr.len();
     let mut gap = len as i32 / 2;
 
@@ -166,9 +151,10 @@ fn shell_sort(arr: &mut [i32]) {
         for i in gap..len as i32 {
             let temp = arr[i as usize];
             let mut j = i;
-
+            count += 1;
             while j >= gap && arr[j as usize - gap as usize] > temp {
                 arr.swap(j as usize, j as usize - gap as usize);
+                count += 1;
                 j -= gap;
             }
 
@@ -176,6 +162,119 @@ fn shell_sort(arr: &mut [i32]) {
         }
         gap /= 2;
     }
+    count
+}
+
+fn hibbard_sort(arr: &mut [i32]) -> usize {
+    let mut count = 0;
+    let len = arr.len();
+    let mut k = (((len as f64).ln()) / (2f64.ln())).floor() as u32;
+    let mut gap = 2i32.pow(k) - 1;
+    while gap > 0 {
+        for i in gap..len as i32 {
+            let temp = arr[i as usize];
+            let mut j = i;
+            count += 1;
+            while j >= gap && arr[j as usize - gap as usize] > temp {
+                arr.swap(j as usize, j as usize - gap as usize);
+                count += 1;
+                j -= gap;
+            }
+
+            arr[j as usize] = temp;
+        }
+        k -= 1;
+        gap = 2i32.pow(k) - 1;
+    }
+    count
+}
+
+fn knuth_sort(arr: &mut [i32]) -> usize {
+    let mut count = 0;
+    let len = arr.len();
+    let mut gap: i32 = 1;
+
+    let mut i = 0;
+    while gap < len as i32 / 3 {
+        gap = (3i32.pow(i) + 1) / 2;
+        i += 1;
+    }
+
+    while gap > 0 {
+        for i in gap..len as i32 {
+            let temp = arr[i as usize];
+            let mut j = i;
+            count += 1;
+            while j >= gap && arr[j as usize - gap as usize] > temp {
+                arr.swap(j as usize, j as usize - gap as usize);
+                count += 1;
+                j -= gap;
+            }
+
+            arr[j as usize] = temp;
+        }
+        i -= 1;
+        gap = (3i32.pow(i) + 1) / 2;
+    }
+    count
+}
+
+fn gonnet_sort(arr: &mut [i32]) -> usize {
+    let mut count = 0;
+    let len = arr.len();
+    let mut gap = (len as f64 / 2.2).floor() as i32;
+
+    while gap > 0 {
+        for i in gap..len as i32 {
+            let temp = arr[i as usize];
+            let mut j = i;
+            count += 1;
+            while j >= gap && arr[j as usize - gap as usize] > temp {
+                arr.swap(j as usize, j as usize - gap as usize);
+                count += 1;
+                j -= gap;
+            }
+
+            arr[j as usize] = temp;
+        }
+        gap = (gap as f64 / 2.2).floor() as i32;
+    }
+    count
+}
+
+fn sedgewick_sort(arr: &mut [i32]) -> usize {
+    let mut count = 0;
+    let len = arr.len();
+    let mut gap = 1;
+    let mut i = 0;
+    while gap < (len as i32) / 3 {
+        gap = 4 * 9i32.pow(i) - 9 * 2i32.pow(i) + 1;
+        if i >= 2 {
+            gap += 4i32.pow(i) - 3 * 2i32.pow(i) + 1
+        }
+        i += 1;
+    }
+
+    while gap > 0 {
+        for i in gap..len as i32 {
+            let temp = arr[i as usize];
+            let mut j = i;
+            count += 1;
+            while j >= gap && arr[j as usize - gap as usize] > temp {
+                arr.swap(j as usize, j as usize - gap as usize);
+                count += 1;
+                j -= gap;
+            }
+
+            arr[j as usize] = temp;
+        }
+        i -= 1;
+        gap = 4 * 9i32.pow(i) - 9 * 2i32.pow(i) + 1;
+        if i >= 2 {
+            gap += 4i32.pow(i) - 3 * 2i32.pow(i) + 1
+        }
+    }
+    count
 }
 
 fn heapsort(arr: &mut Vec<i32>) -> usize {
@@ -198,8 +297,8 @@ fn heapsort(arr: &mut Vec<i32>) -> usize {
 
             if arr[max] > arr[root] {
                 arr.swap(root, max);
-                count += 1;
             }
+            count += 1;
             root = max;
         }
         count
